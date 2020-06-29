fcd9cfdcaf637cce8a6117f4a2b022fbf0d1b99d
zuhair <zuhairzbr@gmail.com>
2020-06-30 02:02:56 +0530

    removed unwanted files

--- app/src/main/java/org/chromium/build/BuildHooksConfig.java
+++
@@ -1,4 +0,0 @@
-package org.chromium.build;
-public class BuildHooksConfig {
-    public static final boolean REPORT_JAVA_ASSERT = false;
-}
--- app/src/main/java/org/chromium/chrome/browser/vr/VrBuildConfig.java
+++
@@ -1,4 +0,0 @@
-package org.chromium.chrome.browser.vr;
-public class VrBuildConfig {
-    public static final boolean IS_VR_ENABLED = true;
-}
